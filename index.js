b154bd91ee7113bba16b822883671b65986094e6
JhonaLegitimo <106883918+JhonaLegitimo@users.noreply.github.com>
2026-05-14 11:35:56 -0500

    API request and all working 🐷

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 const form = document.querySelector("form");
 
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const formData = new FormData(form);
+  const hexValue = formData.get("hex-value").slice(1);
+  const mode = formData.get("mode");
+  fetch(`https://www.thecolorapi.com/scheme?hex=${hexValue}&mode=${mode}`)
+    .then((res) => res.json())
+    .then((data) => renderColors(data));
+});
 
-function renderColors() {
-  form.
+function renderColors(data) {
+  const html = data.colors
+    .map(function (color) {
+      return `<div class="color-container">
+        <div class="color" style="background-color: ${color.hex.value}"></div>
+        <p class="hex-value">${color.hex.value}</p>
+    </div>`;
+    })
+    .join("");
+  document.querySelector(".scheme-container").innerHTML = html;
 }
-fetch();
